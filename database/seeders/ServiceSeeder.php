0f71a47c77f2f9028ae21332ec76f84d95f65a82
Sergey Khmelnitsky <adasdkl28@gmail.com>
2026-01-11 15:21:41 +0300

    Add ServiceSeeder to populate services table with Men Haircut, Women Haircut, and Hair Colouring

---
+++ database/seeders/ServiceSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Service;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $services = [
+            [
+                'name' => 'Men Haircut',
+                'description' => 'Professional men\'s haircut service',
+                'is_active' => true,
+            ],
+            [
+                'name' => 'Women Haircut',
+                'description' => 'Professional women\'s haircut service',
+                'is_active' => true,
+            ],
+            [
+                'name' => 'Hair Colouring',
+                'description' => 'Professional hair colouring service',
+                'is_active' => true,
+            ],
+        ];
+
+        foreach ($services as $serviceData) {
+            Service::updateOrCreate(
+                ['name' => $serviceData['name']],
+                $serviceData
+            );
+        }
+    }
+}
