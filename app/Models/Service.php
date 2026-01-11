e090837b62d6da9d396478e031245f9f563bea6c
Sergey Khmelnitsky <adasdkl28@gmail.com>
2026-01-11 18:57:23 +0300

    Add HasFactory and AsSource traits to Service model

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Orchid\Screen\AsSource;
 
 class Service extends Model
 {
+    use HasFactory, AsSource;
     protected $fillable = [
         'name',
         'description',
