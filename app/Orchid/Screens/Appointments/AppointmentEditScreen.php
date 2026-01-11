8eb99c771d01965f1dba1d285ccf27efedf91aaf
Sergey Khmelnitsky <adasdkl28@gmail.com>
2026-01-11 19:23:45 +0300

    Fix AppointmentEditScreen save: get appointment from route parameter

--- app/Orchid/Screens/Appointments/AppointmentEditScreen.php
+++ app/Orchid/Screens/Appointments/AppointmentEditScreen.php
@@ -122,8 +122,10 @@ class AppointmentEditScreen extends Screen
     /**
      * Save the appointment
      */
-    public function save(Request $request, Appointment $appointment = null): \Illuminate\Http\RedirectResponse
+    public function save(Request $request): \Illuminate\Http\RedirectResponse
     {
+        $appointment = $request->route('appointment');
+        
         if ($appointment === null) {
             $appointment = new Appointment();
         }
