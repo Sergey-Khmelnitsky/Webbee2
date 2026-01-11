6c3e98584463cc6fdc21dba75863fea48fb18867
Sergey Khmelnitsky <adasdkl28@gmail.com>
2026-01-11 19:08:14 +0300

    Add HasFactory and AsSource traits to Appointment model

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Orchid\Screen\AsSource;
 
 class Appointment extends Model
 {
-    use SoftDeletes;
+    use HasFactory, AsSource, SoftDeletes;
 
     protected $fillable = [
         'service_id',
