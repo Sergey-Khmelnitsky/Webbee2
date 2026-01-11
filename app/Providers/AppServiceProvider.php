37bdd6428af8d8950403d1bae2bc6a98b8c26d11
Sergey Khmelnitsky <adasdkl28@gmail.com>
2026-01-11 19:25:57 +0300

    Add explicit route model binding for appointment parameter

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \Illuminate\Support\Facades\Route::bind('appointment', function ($value) {
+            return \App\Models\Appointment::findOrFail($value);
+        });
     }
 }
